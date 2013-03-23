29b06d9837f68be0ae43d1898970c6facb0c1695
yemoo <netman8410@gmail.com>
2013-03-23 20:14:06 +0800

    logger, gearman, cluster, config-read

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,6 @@ module.exports = {
     environment: 'development',
     hostname: '',
     port: 3000,
-    // TODO: log config
-    logPath: cwd + '/log',
     // default view Engine
     viewEngine: 'dust',
     viewCache: true,
@@ -15,5 +13,8 @@ module.exports = {
     processNum: 10,
     // folder setting  WARN: you'd better don't change this setting
     views: cwd + '/views',
-    controllers: cwd + '/controllers'
+    controllers: cwd + '/controllers',
+    configPath: cwd + '/config',
+    // cookie/session secret key
+    secretKey: 'ifchange'
 };
---
+++ config/viewengine.js
@@ -0,0 +1,8 @@
+module.exports = {
+	'dust': function(engines) {
+		return engines.dust;
+	},
+	'handlebars': function(engines) {
+		return engines.handlebars;
+	}
+};
