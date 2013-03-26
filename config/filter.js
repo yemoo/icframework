1af82e1701cb8f3da839d9e0bbd631a6d6e62d5b
yemoo <netman8410@gmail.com>
2013-03-26 16:25:54 +0800

    fix some format issue and remove inflection node_module

--- config/filter.js
+++ config/filter.js
@@ -1,4 +1,4 @@
 module.exports = {
-	before: {},
-	after: {}
+    before: {},
+    after: {}
 }
--- config/viewengine.js
+++ config/viewengine.js
@@ -1,8 +1,8 @@
 module.exports = {
-	'dust': function(engines) {
-		return engines.dust;
-	},
-	'hbs': function(engines) {
-		return engines.handlebars;
-	}
+    'dust': function(engines) {
+        return engines.dust;
+    },
+    'hbs': function(engines) {
+        return engines.handlebars;
+    }
 };
