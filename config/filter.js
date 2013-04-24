0825ccfc91291c24d0ede49a2029c6a59c2d4dbd
yemoo <netman8410@gmail.com>
2013-04-24 23:31:25 +0800

    add req template vars

--- config/filter.js
+++ config/filter.js
@@ -1,15 +1,24 @@
 // config before/after filter functions
 // just like write controller.action
+var reqKeys = ['params', 'query', 'body', 'cookies', 'signedCookies',
+    'ip', 'ips', 'protocol', 'domain', 'path', 'host', 'xhr', 'url', 'originalUrl', 'method', 'originalMethod',
+    'session', 'headers', 'httpVersion'];
+
 module.exports = {
     before: {
         // for controller level template variables
         'LOCALS': function(req, res) {
-            var locals = this._LOCALS;
+            var locals = this._LOCALS, reqData = {};
             if (typeof locals == 'object') {
                 Object.keys(locals).forEach(function(key) {
                     res.locals[key] = locals[key];
                 });
             }
+
+            reqKeys.forEach(function(key){
+                reqData[key] = req[key];
+            });
+            res.locals['req'] = reqData;
         }
     },
     after: {}
