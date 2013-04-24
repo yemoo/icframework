6f3c01bb461aff0ac7f6a46aa8287463917b4288
yemoo <netman8410@gmail.com>
2013-04-24 22:33:52 +0800

    update package.json

--- config/filter.js
+++ config/filter.js
@@ -1,16 +1,16 @@
 // config before/after filter functions
 // just like write controller.action
 module.exports = {
-	before: {
-		// for controller level template variables
-		'LOCALS': function(req, res){
-			var locals = this._LOCALS;
-			if(typeof locals == 'object'){
-				Object.keys(locals).forEach(function(key){
-					res.locals[key] = locals[key];
-				});
-			}
-		}
-	},
-	after: {}
+    before: {
+        // for controller level template variables
+        'LOCALS': function(req, res) {
+            var locals = this._LOCALS;
+            if (typeof locals == 'object') {
+                Object.keys(locals).forEach(function(key) {
+                    res.locals[key] = locals[key];
+                });
+            }
+        }
+    },
+    after: {}
 }
